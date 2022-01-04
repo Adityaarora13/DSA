19205536cea703ccbe4080fb4818d9e14d89633e
Aditya <aparora13@gmail.com>
2022-01-04 08:41:55 +0530

    DSA- linear search

---
+++ src/array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class array
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int size = sc. nextInt();
+        int marks[] = new int[size];
+        int x = sc.nextInt();
+        for(int i=0;i<marks.length;i++)
+        {
+            marks[i] = sc.nextInt();
+        }
+        for(int i=0;i<marks.length;i++)
+        {
+            if(marks[i]==x)
+            System.out.println(i);
+        }
+    }
+}
