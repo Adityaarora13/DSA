60caf1c5d795a4aa2e2bd81fb917ffb7ccc17647
Aditya <aparora13@gmail.com>
2022-01-05 21:32:41 +0530

    inputing a string and replacing a particular character/string with other.

--- src/Array/stringoperations.java
+++ src/Array/stringoperations.java
@@ -6,16 +6,22 @@ import java.util.Scanner;
 public class stringoperations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        String[] arr = new String[size];
-        int count =0;
-        for(int i=0;i<size;i++)
-        {
-            arr[i] = sc.next();
-            count = count + arr[i].length();
-        }
-        System.out.println(Arrays.asList(arr));
-        System.out.println(count);
+        String s = sc.nextLine();
+        String x = sc.next();
+        String y = sc.next();
+        String result = "";
+//        for(int i=0;i<s.length();i++)
+//        {
+//            if((String.valueOf(s.charAt(i)).equals(x)))
+//            {
+//                result = result+y;
+//            }
+//            else result= result + s.charAt(i);
+//        }
+//        System.out.println(result);
+//    }
+        // using replace function
+        System.out.println(s.replace(x, y));
     }
 }
 
