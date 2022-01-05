3f13d7d70c2411fddae5ec58ce230c5e91da9fe8
Aditya <aparora13@gmail.com>
2022-01-05 20:40:51 +0530

    input an array of strings and print combined sum of array

--- src/Array/stringoperations.java
+++ src/Array/stringoperations.java
@@ -1,13 +1,21 @@
 package Array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class stringoperations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        String str = Integer.toString(num);
-        System.out.println(str.length());
+        int size = sc.nextInt();
+        String[] arr = new String[size];
+        int count =0;
+        for(int i=0;i<size;i++)
+        {
+            arr[i] = sc.next();
+            count = count + arr[i].length();
+        }
+        System.out.println(Arrays.asList(arr));
+        System.out.println(count);
     }
 }
 
