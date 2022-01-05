50c7d15874f6abda24f89e56d6856a5de18904dc
Aditya <aparora13@gmail.com>
2022-01-05 19:33:13 +0530

    string array

---
+++ src/Array/stringoperations.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Scanner;
+
+public class stringoperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u')
+                count++;
+        }
+        System.out.println(count);
+    }
+}
+
+
+
