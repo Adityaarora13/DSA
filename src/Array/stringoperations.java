4316cf3d2dc104d1cfc0428d138aeef59dac5be0
Aditya <aparora13@gmail.com>
2022-01-05 19:44:58 +0530

    Integer or character String elements sum.

--- src/Array/stringoperations.java
+++ src/Array/stringoperations.java
@@ -6,12 +6,12 @@ public class stringoperations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        int count = 0;
+        int sum = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u')
-                count++;
+            System.out.println(s.charAt(i)-48);
+            sum=sum + (s.charAt(i)-48);
         }
-        System.out.println(count);
+        System.out.println("sum = "+sum);
     }
 }
 
