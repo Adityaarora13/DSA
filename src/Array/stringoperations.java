d0e60a849de15253fb95d06b219abdeece60c131
Aditya <aparora13@gmail.com>
2022-01-05 20:11:35 +0530

    number to string conversion

--- src/Array/stringoperations.java
+++ src/Array/stringoperations.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class stringoperations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
-        if (s1.compareTo(s2) == 0)
-            System.out.println("strings are equal");
-        else
-            System.out.println("strings are not equal");
+        int num = sc.nextInt();
+        String str = Integer.toString(num);
+        System.out.println(str.length());
     }
 }
 
