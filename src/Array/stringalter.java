cbed4df66855bc5244c5ce17735b50d23cfe1405
Aditya <aparora13@gmail.com>
2022-01-05 22:11:12 +0530

    email id to username

---
+++ src/Array/stringalter.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.Scanner;
+
+public class stringalter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String username="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='@')
+                break;
+            else username = username + s.charAt(i);
+        }
+        System.out.println(username);
+    }
+}
