bfd04f2da13e933d224768f653cd4f993c9af323
Aditya <aparora13@gmail.com>
2022-01-15 17:42:44 +0530

    remove consecutive duplicate in an array

---
+++ src/Array/jan12.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Scanner;
+
+public class jan12 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int n= s.nextInt();
+        int fact=1,count=0;
+        for (int i = n; i >0 ; i--) {
+            fact = fact*i;
+        }
+        System.out.println(fact);
+        String str =Integer.toString(fact);
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '0') {
+                count += 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
